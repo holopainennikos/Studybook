ec024507805c6cb4ed1018ea3dfb4371426cb91b
Niko Holopainen <nikosanteri.holopainen@gmail.com>
2024-08-16 23:09:32 +0300

    made a classroom or i guess a class-personnel js class, currently does nothing

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+function renderClassroom() {
+
+  let classroomHTML = '';
+
+  classroom.forEach((student) => {
+    classroomHTML += `
+       `;
+
+  });
+
+
+  document.querySelector('.js-class-personnel').innerHTML = classroomHTML;
+
+}
