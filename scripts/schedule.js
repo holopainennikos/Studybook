7b2b1761d534a57c2e081da72db81cedadcf2d78
Niko Holopainen <nikosanteri.holopainen@gmail.com>
2024-08-22 23:12:01 +0300

    the schedule html is almost ready now

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -1,10 +1,15 @@
 
 
 let schedule = {
-  period1: "",
-  period2: "",
-  period3: "",
-  period4: "",
-  period5: "",
-  period6: ""
+  id: "1",
+  year: 1,
+  classLetter: "A",
+  groupLetter: "B",
+  period1: "Maths",
+  period2: "Finnish",
+  period3: "English",
+  period4: "Music",
+  period5: "History",
+  period6: "Physical education",
+  period7: "Physical eduaction"
 }
