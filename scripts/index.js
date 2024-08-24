c45a468d07b87a019faee5ffe732ae274c7b6ea2
Niko Holopainen <nikosanteri.holopainen@gmail.com>
2024-08-24 22:08:30 +0300

    homework now generates via js

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,27 @@ function renderClassroom() {
   document.querySelector('.js-class-personnel').innerHTML = classroomHTML;
 
 }
+
+document.querySelector('.js-homework-container').innerHTML = `
+  <div class="homework-subject">
+    <img class="homework-image" src="images/studybook logo.png">
+    <div class="homework-subject-name">Math</div>
+    <div class="homework-content">The "cottage" from chapter 42</div>
+  </div>
+  <div class="homework-subject">
+    <img class="homework-image" src="images/studybook logo.png">
+    <div class="homework-subject-name">Finnish</div>
+    <div class="homework-content">
+      <p>
+        Exercise 2, 3 and 5 from page 92 from the work book 
+      </p>
+      <p>
+        Read pages 132 and 133 from the other book
+      </p>
+    </div>
+  </div>
+  <div class="homework-subject">
+    <img class="homework-image" src="images/studybook logo.png">
+    <div class="homework-subject-name">English</div>
+  </div>
+`;
